7cdd1e7338a24aa3a633e38d8c240e72b70a179e
George <george.hamann@gmail.com>
2024-04-02 20:05:27 -0300

    Exercicio feito em aula

--- src/ListaDeExercicios/Exercicio07/Program.cs
+++ src/ListaDeExercicios/Exercicio07/Program.cs
@@ -9,7 +9,31 @@
              * Exemplo de entrada: A = 5, B = 3, C = 10
              * Exemplo de saída: A + B é menor que C.
              */
-            Console.WriteLine("Hello, World!");
+
+            int A, B, C;
+
+            Console.WriteLine("Entre com valor inteiro para A:");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entre com valor inteiro para B:");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entre com valor inteiro para C:");
+            C = Convert.ToInt32(Console.ReadLine());
+
+            int somaAeB = A + B;
+
+            string saida = "A + B é menor que C";
+
+            //if (somaAeB > C)
+            //{
+            //    saida = "A + B é maior que C";
+            //}
+
+            saida = somaAeB > C ? "A + B é maior que C" : saida;
+
+            Console.WriteLine(saida);
+
         }
     }
 }
